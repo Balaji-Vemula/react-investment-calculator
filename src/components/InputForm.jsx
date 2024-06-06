41a2dbd6b77aba12d4af26be5a90b2e3d7728fbf
vemula <vemulabalaji1998@gmail.com>
2024-06-06 23:10:01 +0530

    Added input functionality with state

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,26 +1,76 @@
-import React from "react";
+import { useState } from "react";
 
 export default function InputForm() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 30000,
+    annualInvestment: 3000,
+    expectedReturn: 6,
+    duration: 3,
+  });
+
+  const handleInputChange = (inputIdentifier, newValue) => {
+    setUserInput((prevUserInput) => ({
+      ...prevUserInput,
+      [inputIdentifier]: newValue,
+      // Here we are using square brackets to fetch the string in the variable inputIdentifier
+      //  or else using directly the variable without square brackets creates the literal varaible name
+      //  as a key not takes the string stored in it for the key
+    }));
+    console.log(userInput)
+  };
+
   return (
     <section id="user-input">
       <div className="input-group">
         <p>
           <label htmlFor="initialInvestment">Initial Investment</label>
-          <input type="number" id="initialInvestment" required />
+          <input
+            type="number"
+            value={userInput.initialInvestment}
+            onChange={(event) =>
+              handleInputChange("initialInvestment", event.target.value)
+            }
+            id="initialInvestment"
+            required
+          />
         </p>
         <p>
           <label htmlFor="annualInvestment">Annual Investment</label>
-          <input type="number" id="annualInvestment" required />
+          <input
+            type="number"
+            id="annualInvestment"
+            value={userInput.annualInvestment}
+            onChange={(event) =>
+              handleInputChange("annualInvestment", event.target.value)
+            }
+            required
+            />
         </p>
       </div>
       <div className="input-group">
         <p>
           <label htmlFor="expectedReturn">Expected Return</label>
-          <input type="number" id="expectedReturn" required />
+          <input
+            type="number"
+            id="expectedReturn"
+            value={userInput.expectedReturn}
+            onChange={(event) =>
+              handleInputChange("expectedReturn", event.target.value)
+              }
+              required
+              />
         </p>
         <p>
           <label htmlFor="duration">Duration</label>
-          <input type="number" id="duration" required />
+          <input
+            type="number"
+            id="duration"
+            value={userInput.duration}
+            onChange={(event) =>
+              handleInputChange("duration", event.target.value)
+            }
+            required
+          />
         </p>
       </div>
     </section>
